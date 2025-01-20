b8ac2f5b060c73a9085a26f8ecc94f6fabb22e9f
임강호 <gksmfthsus35@naver.com>
2025-01-20 14:21:25 +0900

    feat: 초기세팅(BE)

---
+++ backend/src/main/java/com/conkiri/ConkiriApplication.java
@@ -0,0 +1,13 @@
+package com.conkiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConkiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConkiriApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/conkiri/ConkiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conkiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConkiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

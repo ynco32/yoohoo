dac96f33a70303e8823ad6dfc5dded1d80254b46
김미나 <minal2576@naver.com>
2025-01-23 11:17:35 +0900

    feat: 더미 사진 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="w-mobile h-mobile relative overflow-hidden bg-primary-main">
+      {/* Hero 섹션 */}
+      <div className="p-md">
+        <Image 
+          src="/images/loading.gif"  // public 폴더 기준 경로
+          alt="Logo"
+          width={200}  
+          height={120}
+          priority  // LCP(Largest Contentful Paint) 최적화
+        />
+      </div>
+    </div>
+  );
 }

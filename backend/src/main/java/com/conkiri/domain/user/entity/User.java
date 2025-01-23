96f808c0d8882a3b05f6167bb30f6a13f130e660
임강호 <gksmfthsus35@naver.com>
2025-01-23 10:50:55 +0900

    feat: user 엔티티 수정

--- backend/src/main/java/com/conkiri/domain/user/entity/User.java
+++ backend/src/main/java/com/conkiri/domain/user/entity/User.java
@@ -1,5 +1,7 @@
 package com.conkiri.domain.user.entity;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,9 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Entity
 @Getter
 @Table
@@ -35,4 +41,18 @@ public class User {
 
 	@Column(name = "tier", length = 100)
 	private String tier;
+
+	@Column(nullable = false, length = 20)
+	@ColumnDefault("'KAKAO'")
+	private String provider;
+
+	@Column(name = "provider_id", nullable = false)
+	private String providerId;
+
+	@Column(name = "profile_url")
+	private String profileUrl;
+
+	public void updateNickname(String nickname) {
+		this.nickname = nickname;
+	}
 }

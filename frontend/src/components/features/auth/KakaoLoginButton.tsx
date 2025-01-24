7dd8d25feca6e24764c0bb3d1ea92ea9e525e870
김소운 <sozzykim@naver.com>
2025-01-24 13:18:31 +0900

    style: 오타 수정정

--- frontend/src/components/features/auth/KakaoLoginButton.tsx
+++ frontend/src/components/features/auth/KakaoLoginButton.tsx
@@ -6,7 +6,7 @@ export default function KakaoLoginButton(){
     }
     return(
     <button onClick={handleKakaoLogin}  className="flex items-center justify-center w-[300px] bg-[#FEE500] py-3 px-4 rounded-lg hover:bg-[#FEE500]/90 transition-colors">
-    카카오로 시작작하기
+    카카오로 시작하기
     </button>
     )
 }

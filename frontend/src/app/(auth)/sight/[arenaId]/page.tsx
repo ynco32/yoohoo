f0ce3709c71e80aa9f3b274e9a22c3494ba5888b
최윤지 <ynco32@gmail.com>
2025-01-25 12:49:41 +0900

    feat: 공연장과 무대 유형 선택 시 구역 선택 페이지 이동 구현

---
+++ frontend/src/app/(auth)/sight/[arenaId]/page.tsx
@@ -0,0 +1,10 @@
+import ArenaList from '@/components/features/sight/ArenaList';
+
+export default function Sight() {
+  return (
+    <div className="pt-16">
+      {/* 테스트용  */}
+      <div>/sights/areanId/</div>
+    </div>
+  );
+}

e9429cea0742d7a3c20808998be2b1bc7c70416a
최윤지 <ynco32@gmail.com>
2025-01-26 00:58:30 +0900

    feat: sight/[arenaId] 페이지 화면에 스크랩 버튼 추가

--- frontend/src/app/(auth)/sight/[arenaId]/page.tsx
+++ frontend/src/app/(auth)/sight/[arenaId]/page.tsx
@@ -1,10 +1,15 @@
 import SectionWrapper from '@/components/features/sight/SectionWrapper';
-import { sections } from '@/types/sections';
+import { ScrapButton } from '@/components/ui/ScrapButton';
 
 export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="pt-16"></div>
+      <div className="container relative mx-auto px-4">
+        {' '}
+        <ScrapButton isScrapMode={false} />
+      </div>
+
       <div className="container mx-auto flex min-h-screen items-center justify-center px-4 py-8">
         <SectionWrapper />
       </div>

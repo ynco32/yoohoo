4e58506fa0609bddd2777fe47ae214bea84b7cb5
최윤지 <ynco32@gmail.com>
2025-01-26 01:33:06 +0900

    feat: sight/[arenaId] 페이지에 스크랩 모드 구현

--- frontend/src/app/(auth)/sight/[arenaId]/page.tsx
+++ frontend/src/app/(auth)/sight/[arenaId]/page.tsx
@@ -1,17 +1,11 @@
-import SectionWrapper from '@/components/features/sight/SectionWrapper';
-import { ScrapButton } from '@/components/ui/ScrapButton';
+import { ScrapMode } from '@/components/features/sight/ScrapMode';
 
 export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="pt-16"></div>
-      <div className="container relative mx-auto px-4">
-        {' '}
-        <ScrapButton isScrapMode={false} />
-      </div>
-
       <div className="container mx-auto flex min-h-screen items-center justify-center px-4 py-8">
-        <SectionWrapper />
+        <ScrapMode />
       </div>
     </div>
   );

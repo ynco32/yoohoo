44a570e6e8f5b808e820e023f4e76759774d9611
김수영 <dongaseu@naver.com>
2025-01-27 00:33:29 +0900

    feat: 스크랩 DTO 생성

---
+++ backend/src/main/java/com/conkiri/domain/view/dto/response/ScrapSeatDetailResponseDTO.java
@@ -0,0 +1,10 @@
+package com.conkiri.domain.view.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class ScrapSeatDetailResponseDTO {
+}

045297519842ac8b0accdba6fb748524eca73337
최윤지 <ynco32@gmail.com>
2025-01-26 15:07:00 +0900

    feat: 동적 라우팅으로 arenaId 가지고 오도록 변경

--- frontend/src/components/features/sight/ScrapMode.tsx
+++ frontend/src/components/features/sight/ScrapMode.tsx
@@ -3,9 +3,12 @@ import { useState } from 'react';
 import { StarButton } from '@/components/ui/StarButton';
 import { SectionList } from './SectionList';
 import { sections } from '@/types/sections';
+import { useParams } from 'next/navigation';
 
 export const ScrapMode = () => {
   const [isScrapMode, setIsScrapMode] = useState(false);
+  const params = useParams();
+  const currentArenaId = Number(params.arenaId);
 
   const handleScrapModeChange = (isScrap: boolean) => {
     setIsScrapMode(isScrap);
@@ -14,7 +17,7 @@ export const ScrapMode = () => {
   return (
     <>
       <StarButton onScrapModeChange={handleScrapModeChange} />
-      <SectionList arenaId={1} isScrapMode={isScrapMode} />
+      <SectionList arenaId={currentArenaId} isScrapMode={isScrapMode} />
     </>
   );
 };

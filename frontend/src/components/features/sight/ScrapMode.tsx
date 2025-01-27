7ff53f69a49cdb9da41828111f0ef16d958937dd
최윤지 <ynco32@gmail.com>
2025-01-27 23:07:27 +0900

    feat: 좌석 스크랩 모드 추가 구현

--- frontend/src/components/features/sight/ScrapMode.tsx
+++ frontend/src/components/features/sight/ScrapMode.tsx
@@ -2,13 +2,14 @@
 import { useState } from 'react';
 import { StarButton } from '@/components/ui/StarButton';
 import { SectionList } from './SectionList';
-import { sections } from '@/types/sections';
+import { SeatList } from './SeatList';
 import { useParams } from 'next/navigation';
 
 export const ScrapMode = () => {
   const [isScrapMode, setIsScrapMode] = useState(false);
   const params = useParams();
   const currentArenaId = Number(params.arenaId);
+  const currentSectionId = params.sectionId;
 
   const handleScrapModeChange = (isScrap: boolean) => {
     setIsScrapMode(isScrap);
@@ -17,7 +18,15 @@ export const ScrapMode = () => {
   return (
     <>
       <StarButton onScrapModeChange={handleScrapModeChange} />
-      <SectionList arenaId={currentArenaId} isScrapMode={isScrapMode} />
+      {!currentSectionId ? (
+        // sight/[arenaId] 경로에서는 섹션 목록
+        <SectionList arenaId={currentArenaId} isScrapMode={isScrapMode} />
+      ) : (
+        // sight/[arenaId]/[sectionId] 경로에서는 좌석 목록
+        <SeatList isScrapMode={isScrapMode} />
+      )}
     </>
   );
 };
+
+export default ScrapMode;

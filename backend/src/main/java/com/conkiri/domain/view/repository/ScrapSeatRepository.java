6e926d6fc2c1eeb2b071f45c27c4955b2e1a93ed
김수영 <dongaseu@naver.com>
2025-01-29 17:38:23 +0900

    feat: 스크랩이 존재하는 구역 전체 조회 API

--- backend/src/main/java/com/conkiri/domain/view/repository/ScrapSeatRepository.java
+++ backend/src/main/java/com/conkiri/domain/view/repository/ScrapSeatRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.conkiri.domain.base.entity.Arena;
 import com.conkiri.domain.base.entity.Seat;
 import com.conkiri.domain.base.entity.Section;
 import com.conkiri.domain.base.entity.StageType;
@@ -15,4 +16,5 @@ public interface ScrapSeatRepository extends JpaRepository<ScrapSeat, Long> {
 	boolean existsByUserAndSeatAndStageType(User user, Seat seat, StageType stageType);
 	Optional<ScrapSeat> findByUserAndSeatAndStageType(User user, Seat seat, StageType stageType);
 	List<ScrapSeat> findByUserAndStageTypeAndSeat_Section(User user, StageType stageType, Section section);
+	List<ScrapSeat> findByUserAndStageTypeAndSeat_Section_Arena(User user, StageType stageType, Arena arena);
 }

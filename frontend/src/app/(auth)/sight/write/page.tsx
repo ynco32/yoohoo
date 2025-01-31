150837b13eff3deb6812525cb4d7e2da29f147c2
최윤지 <ynco32@gmail.com>
2025-01-31 23:58:32 +0900

    feat: 시야 리뷰 입력 페이지 추가

---
+++ frontend/src/app/(auth)/sight/write/page.tsx
@@ -0,0 +1,6 @@
+import { WriteButton } from '@/components/common/WriteButton';
+import ArenaList from '@/components/features/sight/ArenaList';
+
+export default function ReviewFormPage() {
+  return <></>;
+}

cd01788606199022e12ede59f8a6f46ad0df6468
김소운 <sozzykim@naver.com>
2025-02-01 16:46:02 +0900

    style: 비활성화된 버튼 회색으로 설정

--- frontend/src/components/ui/SubmitButton.tsx
+++ frontend/src/components/ui/SubmitButton.tsx
@@ -4,7 +4,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const SubmitButton = ({ children, ...props }: ButtonProps) => (
-  <button className="h-10 rounded-full bg-primary-main px-6 text-sm" {...props}>
+  <button
+    className="h-10 rounded-full bg-primary-main px-6 text-sm hover:bg-primary-main/80 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:opacity-50"
+    {...props}
+  >
     {children}
   </button>
 );

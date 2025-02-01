6533526a22b4c76955f6e86f5f419599e728ddd1
최윤지 <ynco32@gmail.com>
2025-02-01 21:24:01 +0900

    feat: 폼 컨테이너 컴포넌트 추가

--- frontend/src/app/(auth)/sight/write/page.tsx
+++ frontend/src/app/(auth)/sight/write/page.tsx
@@ -1,6 +1,12 @@
 import { WriteButton } from '@/components/common/WriteButton';
 import ArenaList from '@/components/features/sight/ArenaList';
+import SightReviewForm from '@/components/features/sight/SightReviewForm';
+import FormContainer from '@/components/ui/FormContainer';
 
 export default function ReviewFormPage() {
-  return <></>;
+  return (
+    <FormContainer>
+      <SightReviewForm />
+    </FormContainer>
+  );
 }

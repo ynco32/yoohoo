840c9a8bc54261fc51428cd08cb34cf883a14334
최윤지 <ynco32@gmail.com>
2025-02-03 16:43:48 +0900

    feat: MSW 관련 설정 추가

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,17 +1,21 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: [
+    '../public',
+    './public', // MSW Service Worker를 위해 추가
+  ],
 };
 export default config;

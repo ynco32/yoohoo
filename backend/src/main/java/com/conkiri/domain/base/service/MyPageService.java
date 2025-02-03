3ea5d5db5ed8cd1a504ad2b1cd596887c05c71c7
김수영 <dongaseu@naver.com>
2025-02-03 16:26:09 +0900

    feat: 마이페이지에서 내 후기 전체 조회 API 구현

--- backend/src/main/java/com/conkiri/domain/base/service/MyPageService.java
+++ backend/src/main/java/com/conkiri/domain/base/service/MyPageService.java
@@ -1,5 +1,7 @@
 package com.conkiri.domain.base.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,9 @@ import com.conkiri.domain.sharing.dto.response.SharingResponseDTO;
 import com.conkiri.domain.sharing.repository.SharingRepository;
 import com.conkiri.domain.user.entity.User;
 import com.conkiri.domain.user.repository.UserRepository;
+import com.conkiri.domain.view.dto.response.ReviewResponseDTO;
+import com.conkiri.domain.view.entity.Review;
+import com.conkiri.domain.view.repository.ReviewRepository;
 import com.conkiri.global.exception.user.UserNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,7 @@ public class MyPageService {
 
 	private final SharingRepository sharingRepository;
 	private final UserRepository userRepository;
+	private final ReviewRepository reviewRepository;
 
 	/**
 	 * 마이페이지에서 회원이 작성한 전체 나눔게시글 목록 조회
@@ -44,6 +50,14 @@ public class MyPageService {
 		User user = findUserByIdOrElseThrow(userId);
 
 		return sharingRepository.findScrappedSharingsInMyPage(user, lastSharingId, pageable);
+	}
+
+	// 마이페이지에서 회원이 작성한 후기 게시글 조회
+	public ReviewResponseDTO getReviews(Long userId) {
+		User user = findUserByIdOrElseThrow(userId);
+		List<Review> reviews = reviewRepository.findByUser(user);
+
+		return ReviewResponseDTO.from(reviews);
 	}
 
 	// ====================== 내부 메서드 ======================== //

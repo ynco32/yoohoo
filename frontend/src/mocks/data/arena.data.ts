9c829676a58c0f8b8b82c54b0907570deb90d9fc
최윤지 <ynco32@gmail.com>
2025-02-06 13:16:43 +0900

    test: 더미데이터 추가

--- frontend/src/mocks/data/arena.data.ts
+++ frontend/src/mocks/data/arena.data.ts
@@ -10,4 +10,19 @@ export const mockArenas = [
     arenaName: '고척스카이돔',
     photoUrl: '/images/kspo.png',
   },
+  {
+    arenaId: 3,
+    arenaName: '인스파이어 아레나나',
+    photoUrl: '/images/kspo.png',
+  },
+  {
+    arenaId: 4,
+    arenaName: '체조경기장',
+    photoUrl: '/images/kspo.png',
+  },
+  {
+    arenaId: 5,
+    arenaName: '화정체육관',
+    photoUrl: '/images/kspo.png',
+  },
 ];

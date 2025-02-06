317a61c46b195431ea6f4f6c32256a7470187ec4
김소운 <sozzykim@naver.com>
2025-02-06 20:33:09 +0900

    feat: 구역 관련 공지 팝업 및 스토리 북 생성

--- frontend/src/components/features/ticketing/AreaPopup.tsx
+++ frontend/src/components/features/ticketing/AreaPopup.tsx
@@ -1,20 +1,27 @@
 import Popup from '@/components/ui/Popup';
 import PopupButton from '@/components/ui/PopupButton';
 
-export default function AreaPopup() {
+interface AreaPopupProps {
+  onClose: () => void;
+  onMove: () => void;
+  isOpen: boolean;
+}
+export default function AreaPopup({ onClose, onMove, isOpen }: AreaPopupProps) {
+  if (!isOpen) return null;
+
   //// 더미 데이터
   const Area: string = '2층 E 구역';
   /////
   return (
     <Popup className="text-center">
       <div className="text-center text-xl font-bold">선택 구역</div>
-      <div className="bg-gray-50">{Area}</div>
-      <div>
-        상세 구역 잔여좌석 현황이 제공되지 않는 상품입니다. - css 수정 예정
+      <div className="m-1 bg-gray-50 p-3">{Area}</div>
+      <div className="text-caption1 text-gray-500">
+        상세 구역 잔여좌석 현황이 <br /> 제공되지 않는 상품입니다.
       </div>
-      <div className="flex">
-        <PopupButton>닫기</PopupButton>
-        <PopupButton>이동</PopupButton>
+      <div className="mt-3 flex justify-between justify-evenly gap-3 border-t pt-3">
+        <PopupButton onClick={onClose}>닫기</PopupButton>
+        <PopupButton onClick={onMove}>이동</PopupButton>
       </div>
     </Popup>
   );

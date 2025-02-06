a606476c281084e7b181bc9519ad3bb3a8791500
최윤지 <ynco32@gmail.com>
2025-02-06 09:51:21 +0900

    style: 핸들러 파일 기능별 분리

---
+++ frontend/src/mocks/data/arena.data.ts
@@ -0,0 +1,13 @@
+// data/arena.data.ts
+export const mockArenas = [
+  {
+    arenaId: 1,
+    arenaName: '올림픽체조경기장',
+    photoUrl: '/images/kspo.png',
+  },
+  {
+    arenaId: 2,
+    arenaName: '고척스카이돔',
+    photoUrl: '/images/kspo.png',
+  },
+];
---
+++ frontend/src/mocks/index.ts
@@ -0,0 +1,3 @@
+import { sightHandlers } from './handler/sight.handler';
+
+export const handlers = [...sightHandlers];

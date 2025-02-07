724b2db6c50d2cdb3b06695db13d3b07655084ce
최윤지 <ynco32@gmail.com>
2025-02-07 16:55:53 +0900

    feat: strict-boolean-expressions 규칙 제거

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -12,16 +12,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// 설정을 변수에 할당
 const config = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      // [TypeScript] 타입 정보를 사용하도록 파서 옵션 설정
       parser: typescriptParser,
       parserOptions: {
-        project: './tsconfig.eslint.json', // ESLint 전용 TypeScript 설정 파일로 변경
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
         sourceType: 'module',
       },
@@ -34,19 +32,11 @@ const config = [
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/strict-boolean-expressions': [
-        2,
-        {
-          allowString: false,
-          allowNumber: false,
-        },
-      ],
       '@next/next/no-html-link-for-pages': 'error',
       '@next/next/no-img-element': 'error',
       'react/no-unescaped-entities': 'off',
       'react/display-name': 'off',
     },
-    // [추가] TypeScript 플러그인 설정
     plugins: {
       '@typescript-eslint': typescriptPlugin,
     },
@@ -55,10 +45,9 @@ const config = [
       'src/**/*.d.ts',
       '.next/**/*',
       'out/**/*',
-      'tailwind.config.ts', // 테일윈드 추가
+      'tailwind.config.ts',
     ],
   },
 ];
 
-// 변수를 export
 export default config;

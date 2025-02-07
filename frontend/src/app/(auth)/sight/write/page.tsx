24bed4a24250172e342427d6f04fa8870a43bcf5
최윤지 <ynco32@gmail.com>
2025-02-07 23:39:17 +0900

    feat: 불필요한 부모 컴포넌트 제거

--- frontend/src/app/(auth)/sight/write/page.tsx
+++ frontend/src/app/(auth)/sight/write/page.tsx
@@ -1,10 +1,10 @@
-import { SightReviewFormContainer } from '@/components/features/sight/form/SightReviewFormContainer';
+import SightReviewForm from '@/components/features/sight/form/SightReviewForm';
 import FormContainer from '@/components/ui/FormContainer';
 
 export default function ReviewFormPage() {
   return (
     <FormContainer>
-      <SightReviewFormContainer />
+      <SightReviewForm />
     </FormContainer>
   );
 }

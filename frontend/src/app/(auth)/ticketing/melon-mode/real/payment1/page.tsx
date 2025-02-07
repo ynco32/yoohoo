9473e55511208a30e74c82fe3704525a10edb3e9
김소운 <sozzykim@naver.com>
2025-02-07 16:08:49 +0900

    design: 다음 버튼이 폰 화면을 넘치는 것 수정

--- frontend/src/app/(auth)/ticketing/melon-mode/real/payment1/page.tsx
+++ frontend/src/app/(auth)/ticketing/melon-mode/real/payment1/page.tsx
@@ -13,14 +13,16 @@ export default function TicketingPage() {
   const [quantity, setQuantity] = useState(1);
 
   return (
-    <div className="relative mx-auto h-screen max-w-md bg-gray-50">
-      <StepIndicator currentStep={2} />
-      <TicketInfo />
-      <DiscountSection />
-      <TicketPrice quantity={quantity} setQuantity={setQuantity} />
-      <PriceDetail quantity={quantity} />
-      <div className="fixed bottom-0 w-full max-w-md">
-        <Link href="payment1/payment2">
+    <div className="flex h-full flex-col bg-gray-50">
+      <div className="flex-1 pb-16">
+        <StepIndicator currentStep={2} />
+        <TicketInfo />
+        <DiscountSection />
+        <TicketPrice quantity={quantity} setQuantity={setQuantity} />
+        <PriceDetail quantity={quantity} />
+      </div>
+      <div className="fixed bottom-0 w-full max-w-[430px]">
+        <Link href="payment1/payment2" className="block">
           <TicketingBillButton>다음</TicketingBillButton>
         </Link>
       </div>

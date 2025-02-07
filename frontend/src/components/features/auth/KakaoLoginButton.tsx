29c2cf63dba59e7f84395c723f0d06b39aac4043
김소운 <sozzykim@naver.com>
2025-02-07 13:28:39 +0900

    design: 로그인 화면 디자인 입히기

--- frontend/src/components/features/auth/KakaoLoginButton.tsx
+++ frontend/src/components/features/auth/KakaoLoginButton.tsx
@@ -1,13 +1,17 @@
 import { KAKAO_AUTH_URL } from '@/lib/constans/auth';
 
-export default function KakaoLoginButton() {
+interface KakaoLoginButtonProps {
+  className: string;
+}
+
+export default function KakaoLoginButton({ className }: KakaoLoginButtonProps) {
   const handleKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
   return (
     <button
       onClick={handleKakaoLogin}
-      className="flex w-[300px] items-center justify-center rounded-lg bg-[#FEE500] px-4 py-3 transition-colors hover:bg-[#FEE500]/90"
+      className={`flex w-[300px] items-center justify-center rounded-lg bg-[#FEE500] px-4 py-3 transition-colors hover:bg-[#FEE500]/90 ${className}`}
     >
       카카오로 시작하기
     </button>

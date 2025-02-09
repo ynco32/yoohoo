838fa9bc5cb02cb34ffa052f6fdd213aef7cfcee
최윤지 <ynco32@gmail.com>
2025-02-09 17:23:10 +0900

    refactor: 불필요한 컴포넌트 제거

--- frontend/src/app/(auth)/sight/write/page.tsx
+++ frontend/src/app/(auth)/sight/write/page.tsx
@@ -1,10 +1,5 @@
 import SightReviewForm from '@/components/features/sight/form/SightReviewForm';
-import FormContainer from '@/components/ui/FormContainer';
 
 export default function ReviewFormPage() {
-  return (
-    <FormContainer>
-      <SightReviewForm />
-    </FormContainer>
-  );
+  return <SightReviewForm />;
 }

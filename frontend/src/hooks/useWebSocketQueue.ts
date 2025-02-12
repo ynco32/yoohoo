dbeac67ba8340fc676a28270feb5120c4017d241
김소운 <sozzykim@naver.com>
2025-02-12 22:37:45 +0900

    Revert "feat: fe sockJS 코드 추가"
    This reverts commit e6f0116f1a5a34de4cfd8136e5eb291b75ca73e3.

--- frontend/src/hooks/useWebSocketQueue.ts
+++ frontend/src/hooks/useWebSocketQueue.ts
@@ -1,5 +1,4 @@
 // hooks/useWebSocketQueue.ts
-import SockJS from 'sockjs-client';
 import { AxiosError } from 'axios';
 import { useState, useRef, useEffect } from 'react';
 import { Client, IMessage } from '@stomp/stompjs';
@@ -25,9 +24,7 @@ export const useWebSocketQueue = () => {
       return;
     }
     const client = new Client({
-      webSocketFactory: () =>
-        new SockJS('https://i12b207.p.ssafy.io/ticketing-melon'),
-      // brokerURL: 'wss://i12b207.p.ssafy.io/ticketing-melon',
+      brokerURL: 'wss://i12b207.p.ssafy.io/ticketing-melon',
       connectHeaders: {
         Authorization: `Bearer ${getAccessToken()}`,
       },

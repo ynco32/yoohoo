15f4bd438e3cc3101b196949c7c3c22a25919cb3
김소운 <sozzykim@naver.com>
2025-02-12 21:20:08 +0900

    fix: 웹소켓 엑세스 토큰 추가

--- frontend/src/hooks/useWebSocketQueue.ts
+++ frontend/src/hooks/useWebSocketQueue.ts
@@ -12,12 +12,22 @@ export const useWebSocketQueue = () => {
   const [peopleBehind, setPeopleBehind] = useState(0);
   const stompClient = useRef<Client | null>(null);
 
+  const getAccessToken = () => {
+    return document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('access_token='))
+      ?.split('=')[1];
+  };
+
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_DISABLE_WEBSOCKET === 'true') {
       return;
     }
     const client = new Client({
       brokerURL: 'wss://i12b207.p.ssafy.io/ticketing-melon',
+      connectHeaders: {
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
       debug: (str) => console.log('🤝 STOMP: ' + str),
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
@@ -31,7 +41,7 @@ export const useWebSocketQueue = () => {
     client.onConnect = () => {
       console.log('🤝 웹소켓 연결 성공');
 
-      client.subscribe(`/book/waiting-time`, (message: IMessage) => {
+      client.subscribe(`user/book/waiting-time`, (message: IMessage) => {
         console.log('🤝waiting-time 수신된 메세지:', message.body);
         const response = JSON.parse(message.body);
         setQueueNumber(response.position);

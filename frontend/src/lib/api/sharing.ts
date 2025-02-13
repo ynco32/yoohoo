578034a41bfd25e45ac61d80ebf9ba868d27cf12
김미나 <minal2576@naver.com>
2025-02-13 15:34:13 +0900

    fix: 글 등록시 응답 받는 형식 수정

--- frontend/src/lib/api/sharing.ts
+++ frontend/src/lib/api/sharing.ts
@@ -99,17 +99,13 @@ export const sharingAPI = {
 
       formData.append('file', file, file.name);
 
-      const response = await api.post<{ sharingId: number }>(
-        '/api/v1/sharing',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data', // 명시적으로 Content-Type 설정
-          },
-        }
-      );
+      const response = await api.post<number>('/api/v1/sharing', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // 명시적으로 Content-Type 설정
+        },
+      });
 
-      return response.data.sharingId;
+      return response.data;
     } catch (error) {
       if (error instanceof AxiosError && error.response) {
         console.error('상세 에러:', error.response.data);

e6e85d5ed87d8d59e87b8e6c25e4487fe3f37576
최윤지 <ynco32@gmail.com>
2025-02-13 16:55:59 +0900

    fix: 이미지 경로 수정

--- frontend/src/lib/utils/profileCharacter.ts
+++ frontend/src/lib/utils/profileCharacter.ts
@@ -1,12 +1,11 @@
-// 정적 이미지 임포트
-import Level1Image from '@/public/images/profile/level1.png';
-import Level2Image from '@/public/images/profile/level2.png';
-import Level3Image from '@/public/images/profile/level3.png';
-import Level4Image from '@/public/images/profile/level4.png';
-import CatImage from '@/public/images/cat.png';
+import Level1Image from '../../../public/images/profile/level1.png';
+import Level2Image from '../../../public/images/profile/level2.png';
+import Level3Image from '../../../public/images/profile/level3.png';
+import Level4Image from '../../../public/images/profile/level4.png';
+import CatImage from '../../../public/images/cat.png';
 import { StaticImageData } from 'next/image';
 
-export const getUserProfileImage = (userLevel: string): StaticImageData => {
+export const getUserProfileImage = (userLevel: string): string => {
   const idx = Number(userLevel);
   const profileImages: Record<1 | 2 | 3 | 4, StaticImageData> = {
     1: Level1Image,
@@ -16,8 +15,8 @@ export const getUserProfileImage = (userLevel: string): StaticImageData => {
   };
 
   if (idx in profileImages) {
-    return profileImages[idx as 1 | 2 | 3 | 4];
+    return profileImages[idx as 1 | 2 | 3 | 4].src;
   }
 
-  return CatImage;
+  return CatImage.src;
 };

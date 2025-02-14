5a548066ea9c2c9c5421cd95c8e1d00241c3918a
최윤지 <ynco32@gmail.com>
2025-02-14 09:20:09 +0900

    refactor: 시야 리뷰 작성경로 변경

--- frontend/src/app/(auth)/sight/[arenaId]/[stageType]/page.tsx
+++ frontend/src/app/(auth)/sight/[arenaId]/[stageType]/page.tsx
@@ -6,7 +6,7 @@ export default function SectionSelectPage() {
     <main className="h-[calc(100vh-64px)] bg-background-default">
       <div className="container mx-auto flex h-full items-center justify-center px-4 py-8">
         <ScrapMode />
-        <WriteButton path="/sight/write" />
+        <WriteButton path="/sight/reviews/write" />
       </div>
     </main>
   );

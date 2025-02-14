452a724572ffe48d84c1ccda09d571f8d0de74eb
김소운 <sozzykim@naver.com>
2025-02-14 13:23:39 +0900

    fix: 팝업창 이외에 안 눌리도록 수정

--- frontend/src/components/ui/Popup.tsx
+++ frontend/src/components/ui/Popup.tsx
@@ -8,9 +8,9 @@ export default function Popup({ children, className, isOpen }: PopupProps) {
   if (!isOpen) return null;
   return (
     <div
-      className={`${className ?? ''} pointer-events-none fixed inset-0 flex items-center justify-center bg-black bg-opacity-50`}
+      className={`${className ?? ''} fixed inset-0 z-30 flex items-center justify-center bg-black bg-opacity-50`}
     >
-      <div className="pointer-events-auto w-full max-w-[300px] rounded-lg bg-white p-6">
+      <div className="z-50 w-full max-w-[300px] rounded-lg bg-white p-6">
         {children}
       </div>
     </div>

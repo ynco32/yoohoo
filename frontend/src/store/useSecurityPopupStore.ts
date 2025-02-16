2ef8f057d3298527f9afe1a1d10c49818c57c008
김소운 <sozzykim@naver.com>
2025-02-16 15:11:49 +0900

    feat: 보안 팝업 상태 저장 store 생성

---
+++ frontend/src/store/useSecurityPopupStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+interface SecurityPopupStoreProps {
+  onSuccess: boolean;
+  setSecurityPopupState: (onPostpone: boolean) => void;
+}
+
+export const useSecurityPopupStore = create<SecurityPopupStoreProps>((set) => ({
+  onPostpone: false,
+  onSuccess: false,
+  setSecurityPopupState: (onSuccess: boolean) => set({ onSuccess }),
+
+  reset: () => set({ onSuccess: false }),
+}));

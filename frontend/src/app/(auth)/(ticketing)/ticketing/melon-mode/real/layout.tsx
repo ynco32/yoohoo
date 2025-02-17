bbdaedfa22c3f4382ddbe913d3bf9f211c9ae91e
김소운 <sozzykim@naver.com>
2025-02-17 23:59:25 +0900

    fix: 웹소켓 코드 layout 되돌리기

--- frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/real/layout.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/real/layout.tsx
@@ -1,20 +1,35 @@
 // 티켓팅을 위한 웹소켓 레이아웃입니다~!
 
+// 티켓팅을 위한 웹소켓 레이아웃입니다~!
+
 'use client';
 import { useWebSocketQueue } from '@/hooks/useWebSocketQueue';
-import { useEffect } from 'react';
 
 export default function TicketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { disconnect, isConnected } = useWebSocketQueue(); // 티켓팅 관련 페이지들에서만 웹소켓 연결 유지
-  useEffect(() => {
-    return () => {
-      if (isConnected) disconnect();
-    };
-  }, [disconnect, isConnected]);
+  useWebSocketQueue(); // 티켓팅 관련 페이지들에서만 웹소켓 연결 유지
 
   return <>{children}</>;
 }
+
+// 'use client';
+// import { useWebSocketQueue } from '@/hooks/useWebSocketQueue';
+// import { useEffect } from 'react';
+
+// export default function TicketingLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const { disconnect, isConnected } = useWebSocketQueue(); // 티켓팅 관련 페이지들에서만 웹소켓 연결 유지
+//   useEffect(() => {
+//     return () => {
+//       if (isConnected) disconnect();
+//     };
+//   }, [disconnect, isConnected]);
+
+//   return <>{children}</>;
+// }

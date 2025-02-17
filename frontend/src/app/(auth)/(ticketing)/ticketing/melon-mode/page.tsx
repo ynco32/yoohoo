09050fdd3156f125997c9f7c60aa5f7d40858f9a
최윤지 <ynco32@gmail.com>
2025-02-18 00:10:32 +0900

    design: 폰트 변경

--- frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/page.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/page.tsx
@@ -4,7 +4,7 @@ export default function ModeSelect() {
   return (
     <div className="flex h-full flex-col bg-ticketing-bg">
       <div className="flex items-center justify-center px-4 pt-20">
-        <span className="font-title-bold text-text-menu">
+        <span className="text-title-bold text-text-menu">
           연습할 플랫폼을 선택해주세요
         </span>
       </div>
--- frontend/src/app/(auth)/(ticketing)/ticketing/page.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/page.tsx
@@ -5,7 +5,7 @@ export default function PlatformListPage() {
   return (
     <div className="flex h-full flex-col bg-ticketing-bg">
       <div className="flex items-center justify-center px-3 pt-20">
-        <span className="font-title-bold text-text-menu">
+        <span className="text-title-bold text-text-menu">
           연습할 플랫폼을 선택해주세요
         </span>
       </div>

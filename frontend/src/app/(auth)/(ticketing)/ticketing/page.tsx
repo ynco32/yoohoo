6edfdfb9d9073e8c39881129febb54c0988cf192
최윤지 <ynco32@gmail.com>
2025-02-17 21:40:13 +0900

    design: 버튼 디자인 수정

--- frontend/src/app/(auth)/(ticketing)/ticketing/page.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/page.tsx
@@ -3,13 +3,13 @@ import PlatformButtons from '@/components/features/ticketing/PlatformButtons';
 
 export default function PlatformListPage() {
   return (
-    <div className="bg-ticketing-bg h-full">
-      <div className="flex items-center justify-center px-3 py-20">
+    <div className="flex h-full flex-col bg-ticketing-bg">
+      <div className="flex items-center justify-center px-3 pt-20">
         <span className="font-title-bold text-text-menu">
           연습할 플랫폼을 선택해주세요
         </span>
       </div>
-      <div className="container">
+      <div className="-mt-8 flex flex-1 items-center justify-center">
         <PlatformButtons />
       </div>
     </div>

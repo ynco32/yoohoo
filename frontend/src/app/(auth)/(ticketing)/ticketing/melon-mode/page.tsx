81459e80315a0e5478b9e78b83d2cf5af23d98ab
최윤지 <ynco32@gmail.com>
2025-02-18 13:53:39 +0900

    design: 티켓팅 페이지 레이아웃 수정

--- frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/page.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/page.tsx
@@ -3,7 +3,7 @@ import Mode from '@/components/features/ticketing/ModeButtons';
 export default function ModeSelect() {
   return (
     <div className="flex h-full flex-col bg-ticketing-bg">
-      <div className="flex items-center justify-center px-4 pt-20">
+      <div className="flex items-center justify-center px-4 pt-40">
         <span className="text-title-bold text-text-menu">
           모드를 선택해주세요
         </span>
--- frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/practice/page.tsx
+++ frontend/src/app/(auth)/(ticketing)/ticketing/melon-mode/practice/page.tsx
@@ -3,7 +3,7 @@ import PracticeMenu from '@/components/features/ticketing/PracticeMenu';
 export default function Practice() {
   return (
     <div className="flex h-full flex-col bg-ticketing-bg">
-      <div className="flex items-center justify-center px-4 pt-20">
+      <div className="flex items-center justify-center px-4 pt-40">
         <span className="text-title-bold text-text-menu">
           연습 유형을 선택하세요
         </span>

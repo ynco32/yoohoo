fdf91787e30eaeb74656a57e8a295ae5f4863349
최윤지 <ynco32@gmail.com>
2025-02-20 09:56:57 +0900

    design: 페이지 레이아웃 통일

--- frontend/src/app/(auth)/sight/[arenaId]/[stageType]/[sectionId]/[seatId]/page.tsx
+++ frontend/src/app/(auth)/sight/[arenaId]/[stageType]/[sectionId]/[seatId]/page.tsx
@@ -5,7 +5,7 @@ import { WriteButton } from '@/components/common/WriteButton';
 export default function SeatPage() {
   return (
     <main className="flex h-full w-full flex-col">
-      <div className="z-20 mt-8 flex w-full flex-col items-center justify-center">
+      <div className="justify-centerr z-content mt-0 flex w-full flex-col items-center">
         <ScrapMode />
         <SightReviewList />
         <WriteButton path="/sight/reviews/write" />

475552cc9adf3dbbf37993a4c99dfdcfdaea3245
김미나 <minal2576@naver.com>
2025-02-20 10:19:41 +0900

    design: 파비콘 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { AuthGuard } from '@/provider/authGuard';
 import HeaderWrapper from './header-wrapper';
 
 const APP_NAME = '콘끼리'; // 설치되는 이름  
-const APP_DEFAULT_TITLE = '콘끼리🎵 - 콘서트를 더 즐겁게'; // 탭 상단에 뜨는 설명
+const APP_DEFAULT_TITLE = '콘끼리 - 콘서트를 더 즐겁게🎵'; // 탭 상단에 뜨는 설명
 const APP_TITLE_TEMPLATE = '%s | 콘끼리 CONKIRI'; // 각 페이지 뒤에 뜨는 설명
 const APP_DESCRIPTION = '콘서트 티켓팅부터, 관람 후 집 도착까지!'; // 웹사이트 설명
 

248672bfc2728594efea60f4a4d9f337220c8215
김미나 <minal2576@naver.com>
2025-02-20 14:10:42 +0900

    design: SNS 썸네일 절대경로로 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import MSWProvider from '@/provider/MSWProvider';
 import { AuthGuard } from '@/provider/authGuard';
 import HeaderWrapper from './header-wrapper';
 
-const APP_NAME = '콘끼리'; // 설치되는 이름  
+const APP_NAME = '콘끼리'; // 설치되는 이름
 const APP_DEFAULT_TITLE = '콘끼리 - 콘서트를 더 즐겁게🎵'; // 탭 상단에 뜨는 설명
 const APP_TITLE_TEMPLATE = '%s | 콘끼리 CONKIRI'; // 각 페이지 뒤에 뜨는 설명
 const APP_DESCRIPTION = '콘서트 티켓팅부터, 관람 후 집 도착까지!'; // 웹사이트 설명
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     url: 'https://i12b207.p.ssafy.io/',
     images: [
       {
-        url: '/images/thumbnail.png',
+        url: 'https://i12b207.p.ssafy.io/images/thumbnail.png',
         width: 1200,
         height: 630,
         alt: APP_DEFAULT_TITLE,

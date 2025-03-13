883408145edb4fc6d86d4f1c5414be7a15c68b08
Jaidy <hyjgood2000@naver.com>
2025-03-13 16:15:16 +0900

    feat : msw 초기 설정

---
+++ frontend/src/mocks/index.ts
@@ -0,0 +1,14 @@
+// src/mocks/index.ts
+async function initMocks() {
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server');
+    server.listen({ onUnhandledRequest: 'bypass' });
+  } else {
+    const { worker } = await import('./browser');
+    worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+}
+
+export default initMocks;

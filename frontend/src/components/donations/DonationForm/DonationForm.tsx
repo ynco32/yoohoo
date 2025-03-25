3ff5bf1a35dd24e96ca687d2d45eed8b0879db59
김미나 <minal2576@naver.com>
2025-03-25 14:27:16 +0900

    feat: 후원하기 페이지 생성

---
+++ frontend/src/components/donations/DonationForm/DonationForm.tsx
@@ -0,0 +1,13 @@
+type DonationFormProps = {
+  initialShelterId?: number;
+  initialDogId?: number;
+};
+
+export default function DonationForm({
+  initialShelterId,
+  initialDogId,
+}: DonationFormProps) {
+  return (
+  <form></form>
+  );
+}

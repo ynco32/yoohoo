8f737c4d09d074a4a7a2a4e87462bf2f0075f5ac
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-04-01 11:08:44 +0900

    fix: merge conflict

--- backend/src/main/java/com/yoohoo/backend/controller/HomeController.java
+++ backend/src/main/java/com/yoohoo/backend/controller/HomeController.java
@@ -8,25 +8,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-<<<<<<< HEAD
-    // @Value("${kakao.client-id}")
-    // private String kakaoClientId;
-
-    // @Value("${KAKAO_REDIRECT_URI}")
-    // private String kakaoRedirectUri;
-
-    // @GetMapping("/")
-    // public String index(Model model) {
-    //     model.addAttribute("kakaoClientId", kakaoClientId);
-    //     model.addAttribute("kakaoRedirectUri", kakaoRedirectUri);
-    //     return "index";
-    // }
-
-    // @GetMapping("/home")
-    // public String home() {
-    //     return "home"; // home.html을 반환
-    // }
-=======
-
->>>>>>> 7d492cd2b4943b90162585f4183863d178888a6e
 }

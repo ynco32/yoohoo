f7c714e48c268f2ecdc59980c7d56a79b115a720
이대현 <eddy1219@naver.com>
2025-04-07 11:02:13 +0900

    fix: RedisConfig class add redisPassword

--- backend/src/main/java/com/yoohoo/backend/config/RedisConfig.java
+++ backend/src/main/java/com/yoohoo/backend/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,9 +19,14 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${REDIS_PASSWORD}")
+    private String redisPassword;
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setPassword(RedisPassword.of(redisPassword));
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean

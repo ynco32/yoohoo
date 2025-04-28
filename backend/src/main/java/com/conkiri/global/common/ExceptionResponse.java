a7f7058fbd2cdc317f0fded1a3f5f59950537fb0
임강호 <gksmfthsus35@naver.com>
2025-04-28 17:29:32 +0900

    feat: DTO 추가

---
+++ backend/src/main/java/com/conkiri/global/common/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package com.conkiri.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+
+	private String code;
+	private String message;
+}

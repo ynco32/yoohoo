e0702690d89a320c783cf75bda5806019d7afc81
김수영 <dongaseu@naver.com>
2025-05-02 17:26:38 +0900

    Feat: 단일 후기 조회 API 구현

--- backend/src/main/java/com/conkiri/domain/view/controller/ViewController.java
+++ backend/src/main/java/com/conkiri/domain/view/controller/ViewController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.conkiri.domain.view.dto.request.ReviewRequestDTO;
+import com.conkiri.domain.view.dto.response.ReviewDetailResponseDTO;
+import com.conkiri.domain.view.dto.response.ReviewResponseDTO;
 import com.conkiri.domain.view.service.ViewService;
 import com.conkiri.global.auth.token.UserPrincipal;
 import com.conkiri.global.common.ApiResponse;
@@ -33,5 +37,14 @@ public class ViewController {
 		@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
 		return ApiResponse.success(viewService.createReview(reviewRequestDTO, files, userPrincipal.getUser()));
+	}
+
+	// 단일 후기 조회 / 수정할 후기 조회
+	@GetMapping("/reviews/{reviewId}")
+	public ApiResponse<ReviewDetailResponseDTO> getAReview(
+		@PathVariable("reviewId") Long reviewId,
+		@AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+		return ApiResponse.success(viewService.getAReview(reviewId, userPrincipal.getUser()));
 	}
 }

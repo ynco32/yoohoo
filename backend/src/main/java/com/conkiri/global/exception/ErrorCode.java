d63cc31ca2a2d7d56198864df961a8ef16feece5
김수영 <dongaseu@naver.com>
2025-05-02 17:26:18 +0900

    Feat: 에러 코드 생성

--- backend/src/main/java/com/conkiri/global/exception/ErrorCode.java
+++ backend/src/main/java/com/conkiri/global/exception/ErrorCode.java
@@ -60,6 +60,7 @@ public enum ErrorCode {
 	ARENA_NOT_FOUND(HttpStatus.BAD_REQUEST, "공연장을 찾을 수 없습니다."),
 	UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "해당 사용자에게 권한이 없습니다."),
 	SEAT_NOT_FOUND(HttpStatus.BAD_REQUEST, "좌석을 찾을 수 없습니다."),
+	REVIEW_NOT_FOUND(HttpStatus.BAD_REQUEST, "리뷰를 찾을 수 없습니다."),
 
 	// 콘서트
 	DUPLICATE_TICKETING(HttpStatus.CONFLICT, "티켓팅에 참여한 내역이 있어 참여할 수 없습니다."),

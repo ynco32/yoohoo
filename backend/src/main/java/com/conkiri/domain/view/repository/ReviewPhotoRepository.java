6091d157fa68e1627450ee0cadad0bf7d78ffa4e
김수영 <dongaseu@naver.com>
2025-05-05 17:41:57 +0900

    Feat: 내가 작성한 후기 조회 API 구현

--- backend/src/main/java/com/conkiri/domain/view/repository/ReviewPhotoRepository.java
+++ backend/src/main/java/com/conkiri/domain/view/repository/ReviewPhotoRepository.java
@@ -1,8 +1,18 @@
 package com.conkiri.domain.view.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.conkiri.domain.view.entity.Review;
 import com.conkiri.domain.view.entity.ReviewPhoto;
 
 public interface ReviewPhotoRepository extends JpaRepository<ReviewPhoto, Long> {
+
+	List<ReviewPhoto> findAllByReview(Review review);
+
+	@Query("SELECT rp FROM ReviewPhoto rp WHERE rp.review.reviewId IN :reviewIds")
+	List<ReviewPhoto> findAllByReviewIdIn(@Param("reviewIds") List<Long> reviewIds);
 }
--- backend/src/main/java/com/conkiri/domain/view/repository/ReviewRepository.java
+++ backend/src/main/java/com/conkiri/domain/view/repository/ReviewRepository.java
@@ -1,11 +1,13 @@
 package com.conkiri.domain.view.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.conkiri.domain.user.entity.User;
 import com.conkiri.domain.view.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -19,4 +21,12 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	""")
 	Optional<Review> findWithUserConcertSeatById(@Param("reviewId") Long reviewId);
 
+	@Query("""
+    SELECT r FROM Review r
+    JOIN FETCH r.user
+    JOIN FETCH r.concert
+    JOIN FETCH r.seat
+    WHERE r.user = :user
+""")
+	List<Review> findAllWithUserConcertSeatByUser(@Param("user") User user);
 }

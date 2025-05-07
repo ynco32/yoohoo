a634f28ff330a556035837af533a1f109a5e9d7b
임강호 <gksmfthsus35@naver.com>
2025-05-07 17:25:44 +0900

    refactor: 응답 메시지 수정

--- backend/src/main/java/com/conkiri/domain/ticketing/dto/request/TicketingTimeRequestDTO.java
+++ backend/src/main/java/com/conkiri/domain/ticketing/dto/request/TicketingTimeRequestDTO.java
@@ -11,7 +11,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record TicketingTimeRequestDTO(
 	@NotBlank(message = ValidationMessage.NULL_IS_NOT_ALLOWED) String concertName,
-	@NotBlank(message = ValidationMessage.NULL_IS_NOT_ALLOWED) String platform,
+	@NotBlank(message = ValidationMessage.NULL_IS_NOT_ALLOWED) String ticketingPlatform,
 	@NotNull(message = ValidationMessage.NULL_IS_NOT_ALLOWED) LocalDateTime startTime,
 	@NotNull(message = ValidationMessage.NULL_IS_NOT_ALLOWED) LocalDateTime endTime
 ) {

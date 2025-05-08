9e2cad4dab2d493002d74790585e9136a0a5fab5
임강호 <gksmfthsus35@naver.com>
2025-05-08 11:03:43 +0900

    feat: enum 추가

---
+++ backend/src/main/java/com/conkiri/domain/notification/entity/NotificationType.java
@@ -0,0 +1,15 @@
+package com.conkiri.domain.notification.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum NotificationType {
+	TICKETING_DAY("티켓팅 당일"),
+	TICKETING_SOON("티켓팅 1시간 전"),
+	CONCERT_DAY("공연 당일"),
+	CONCERT_SOON("공연 시작 1시간 전");
+
+	private final String description;
+}

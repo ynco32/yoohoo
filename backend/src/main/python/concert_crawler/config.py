912a36ee9df2ee467ddea9dba264a09688b592c5
김소운 <sozzykim@naver.com>
2025-05-08 23:37:59 +0900

    Feat: S3 관련 환경 변수 추가

--- backend/src/main/python/concert_crawler/config.py
+++ backend/src/main/python/concert_crawler/config.py
@@ -12,6 +12,12 @@ OCR_API_URL = "https://oe7fe4lwbg.apigw.ntruss.com/custom/v1/41432/d0512270cb2eb
 OCR_SECRET_KEY = "eG9WbHd3cHhPU1JnemRQYXRtRUFEVXBnWHFSZFFPa1E="  
 OCR_API_KEY = "Edx7NLBFZ7q4W1Je02zTwCWnoWPLZ1SmPcBYha7e"        
 
+# S3 설정
+S3_BUCKET_NAME = "chatbot-capture-images"
+S3_BASE_FOLDER = "chunks"
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY") 
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+S3_REGION = os.getenv("S3_REGION", "ap-northeast-2")
 
 
 # 데이터베이스 설정
@@ -35,3 +41,6 @@ FILTERED_VENUES = [
 
 # 이미지 저장 경로
 TEMP_IMAGE_DIR = "temp_images"
+
+# API 엔드포인트 설정 (Java API)
+API_CONCERT_ENDPOINT = os.getenv("API_CONCERT_ENDPOINT")

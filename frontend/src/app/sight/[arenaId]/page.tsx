eeba5bcaa23a308b7bc5598e8390df1a22eec7a0
ynco32 <ynco32@sogang.ac.kr>
2025-05-09 23:56:40 +0900

    Feat: 선택한 공연장에 따라 구역표 보여지도록 구현

--- frontend/src/app/sight/[arenaId]/page.tsx
+++ frontend/src/app/sight/[arenaId]/page.tsx
@@ -1,16 +1,20 @@
-// app/arena/page.tsx
 import { Suspense } from 'react';
 import styles from './page.module.scss';
 
 import SectionMap from '../_components/SectionMap';
-import Loading from '../_components/Loading';
 
-export default function ArenaPage() {
+interface ArenaPageProps {
+  params: {
+    arenaId: string;
+  };
+}
+
+export default function ArenaPage({ params }: ArenaPageProps) {
   return (
     <div className={styles.container}>
       <div className={styles.sections}>
         <Suspense fallback={<div>구역표 로딩 중...</div>}>
-          <SectionMap />
+          <SectionMap arenaId={params.arenaId} />
         </Suspense>
       </div>
     </div>

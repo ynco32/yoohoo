7cf14b0bdc3d99f66a2fcd1f5740acc8c6d2bcc0
김소운 <sozzykim@naver.com>
2025-05-11 21:22:26 +0900

    Fix: repository 태그 삭제

--- backend/src/main/java/com/conkiri/domain/base/repository/ArtistRepository.java
+++ backend/src/main/java/com/conkiri/domain/base/repository/ArtistRepository.java
@@ -3,12 +3,10 @@ package com.conkiri.domain.base.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.conkiri.domain.base.entity.Artist;
 
-@Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
-    
-    Optional<Artist> findByArtistName(String artistName);
+
+	Optional<Artist> findByArtistName(String artistName);
 }
--- backend/src/main/java/com/conkiri/domain/base/repository/ConcertDetailRepository.java
+++ backend/src/main/java/com/conkiri/domain/base/repository/ConcertDetailRepository.java
@@ -1,17 +1,13 @@
 package com.conkiri.domain.base.repository;
 
-import com.conkiri.domain.base.entity.Concert;
-import com.conkiri.domain.base.entity.ConcertDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.conkiri.domain.base.entity.ConcertDetail;
 
-@Repository
 public interface ConcertDetailRepository extends JpaRepository<ConcertDetail, Long> {
-    // 특정 콘서트에 속한 모든 상세 정보 조회
-    List<ConcertDetail> findByConcert(Concert concert);
-    
-    // 콘서트 ID로 모든 상세 정보 조회
-    List<ConcertDetail> findByConcertConcertId(Long concertId);
+	// // 특정 콘서트에 속한 모든 상세 정보 조회
+	// List<ConcertDetail> findByConcert(Concert concert);
+	//
+	// // 콘서트 ID로 모든 상세 정보 조회
+	// List<ConcertDetail> findConcertByConcertId(Long concertId);
 }

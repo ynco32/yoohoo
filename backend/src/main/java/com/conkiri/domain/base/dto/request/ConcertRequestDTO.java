d01a3923554fed541ecddf55ca39f24afac376c7
김소운 <sozzykim@naver.com>
2025-05-11 21:24:09 +0900

    Fix: null 유효성 검사 추가

--- backend/src/main/java/com/conkiri/domain/base/dto/request/ConcertRequestDTO.java
+++ backend/src/main/java/com/conkiri/domain/base/dto/request/ConcertRequestDTO.java
@@ -1,10 +1,13 @@
 package com.conkiri.domain.base.dto.request;
 
+import com.conkiri.global.exception.ValidationMessage;
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record ConcertCreateRequestDTO(
-    String concertName,
+public record ConcertRequestDTO(
+    @NotBlank(message = ValidationMessage.BLANK_IS_NOT_ALLOWED) String concertName,
     List<String> artists,
     String venueName,
     String photoUrl,
@@ -15,5 +18,5 @@ public record ConcertCreateRequestDTO(
     String noticeImageUrl,
     String noticeText
 ) {
-    
+
 }

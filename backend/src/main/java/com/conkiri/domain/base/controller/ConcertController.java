8e8c84730b87c9088fb6b9e80ce1894463688868
김소운 <sozzykim@naver.com>
2025-05-11 21:23:17 +0900

    Fix: ResponseEntity에서 ApiResponse로 수정

--- backend/src/main/java/com/conkiri/domain/base/controller/ConcertController.java
+++ backend/src/main/java/com/conkiri/domain/base/controller/ConcertController.java
@@ -1,44 +1,42 @@
 package com.conkiri.domain.base.controller;
 
-import com.conkiri.domain.base.dto.request.ConcertCreateRequestDTO;
-import com.conkiri.global.common.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import com.conkiri.domain.base.dto.request.ConcertRequestDTO;
 import com.conkiri.domain.base.service.ConcertService;
-
+import com.conkiri.global.common.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/concert")
 public class ConcertController {
 
-	private final ConcertService concertService;
+    private final ConcertService concertService;
 
-	/**
-	 * 콘서트 목록 조회
-	 * @param concertSearch
-	 * @param lastConcertId
-	 * @return
-	 */
-	// @GetMapping
-	// public ApiResponse<ConcertResponseDTO> getConcertList(
-	// 	@RequestParam(value = "value", required = false) String concertSearch,
-	// 	@RequestParam(value = "last", required = false) Long lastConcertId) {
-	//
-	// 	return ApiResponse.success(concertService.getConcertList(concertSearch, lastConcertId));
-	// }
-
+    /**
+     * 콘서트 목록 조회
+     *
+     * @param concertSearch
+     * @param lastConcertId
+     * @return
+     */
+    // @GetMapping
+    // public ApiResponse<ConcertResponseDTO> getConcertList(
+    // 	@RequestParam(value = "value", required = false) String concertSearch,
+    // 	@RequestParam(value = "last", required = false) Long lastConcertId) {
+    //
+    // 	return ApiResponse.success(concertService.getConcertList(concertSearch, lastConcertId));
+    // }
     @PostMapping
-    public ResponseEntity<ApiResponse<?>> createConcert(@RequestBody ConcertCreateRequestDTO request) {
+    public ApiResponse<Long> createConcert(@Valid @RequestBody ConcertRequestDTO request) {
         Long concertId = concertService.createConcert(request);
-        return ResponseEntity.ok(ApiResponse.success(concertId));
+        return ApiResponse.success(concertId);
     }
 
-	@GetMapping("/checkExists")
-	public ResponseEntity<ApiResponse<?>> checkConcertExists(@RequestParam String concertName) {
-		boolean exists = concertService.checkConcertExists(concertName);
-		return ResponseEntity.ok(ApiResponse.success(exists));
-	}
+    @GetMapping("/checkExists")
+    public ApiResponse<Boolean> checkConcertExists(@RequestParam String concertName) {
+        boolean exists = concertService.checkConcertExists(concertName);
+        return ApiResponse.success(exists);
+    }
 }

0a5e1b7f002367e9ef8b2c0c8bdc86a99c8f0917
김소운 <sozzykim@naver.com>
2025-05-12 09:36:58 +0900

    Refactor: 변수명 정리

--- backend/src/main/python/concert_crawler/api/concert_api.py
+++ backend/src/main/python/concert_crawler/api/concert_api.py
@@ -30,19 +30,25 @@ def save_concert_to_java_api(concert_data):
     """콘서트 데이터를 Java API로 전송"""
     api_url = API_BASE_URL + "/api/v1/concert"  # API 엔드포인트
 
+    concert_name = concert_data.get('title') or concert_data.get('concert_name')
+    venue_name = concert_data.get('place') or concert_data.get('venue')
+    artist_list = concert_data.get('artists', [])
+    if not artist_list and 'artist' in concert_data and concert_data['artist']:
+        artist_list = [concert_data['artist']]
+
     ticketing_platform = concert_data.get('ticketing_platform', 'INTERPARK')
     
     request_data = {
-        "concertName": concert_data.get('concert_name'),
-        "artistName": concert_data.get('artist'),
-        "venueName": concert_data.get('venue'),
+        "concertName": concert_name,
+        "artistName": artist_list,
+        "venueName": venue_name,
         "photoUrl": concert_data.get('poster_url'),
         "advanceReservation": concert_data.get('advance_reservation'),
         "reservation": concert_data.get('reservation'),
         "ticketingPlatform": ticketing_platform,
         "startTimes": concert_data.get('start_times', []),
         "noticeImageUrl": concert_data.get('notice_image_url'),
-        "noticeText": concert_data.get('ocr_text')
+        "noticeText": concert_data.get('ocr_text', '')
     }
     
     try:

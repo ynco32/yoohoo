06b408dd1edb87df78957dfa810c1e18bae4f0b2
최윤지 <ynco32@gmail.com>
2025-05-12 16:22:13 +0900

    Feat: 브로커 경로 변경

--- frontend/src/hooks/useWebSocketQueue.ts
+++ frontend/src/hooks/useWebSocketQueue.ts
@@ -27,7 +27,7 @@ export const useWebSocketQueue = () => {
       return;
     }
     const client = new Client({
-      brokerURL: 'wss://i12b207.p.ssafy.io/ticketing-melon',
+      brokerURL: 'wss://conkiri.shop/ticketing-melon',
       connectHeaders: {
         Authorization: `Bearer ${getAccessToken()}`,
       },

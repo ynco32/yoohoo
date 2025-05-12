c7572dd39ec5fe7fb7fb45b33f42644370786087
임강호 <gksmfthsus35@naver.com>
2025-05-12 23:58:40 +0900

    feat: dto 수정

--- backend/src/main/java/com/conkiri/domain/base/dto/response/ConcertDetailResponseDTO.java
+++ backend/src/main/java/com/conkiri/domain/base/dto/response/ConcertDetailResponseDTO.java
@@ -1,23 +1,14 @@
 package com.conkiri.domain.base.dto.response;
 
-import com.conkiri.domain.base.entity.Concert;
+import java.util.List;
 
 public record ConcertDetailResponseDTO(
 	Long concertId,
 	String concertName,
-	//String artist,
-	//LocalDateTime startTime,
 	String photoUrl,
-	String arena
+	String arenaName,
+	boolean ticketingNotificationEnabled,
+	List<ConcertCastResponseDTO> artists,
+	List<ConcertSessionDTO> sessions
 ) {
-	public static ConcertDetailResponseDTO from(Concert concert) {
-		return new ConcertDetailResponseDTO(
-			concert.getConcertId(),
-			concert.getConcertName(),
-			//concert.getArtist(),
-			//concert.getStartTime(),
-			concert.getPhotoUrl(),
-			concert.getArena().getArenaName()
-		);
-	}
 }

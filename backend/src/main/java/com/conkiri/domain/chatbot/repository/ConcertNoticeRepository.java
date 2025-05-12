84fcab8087d5eba5f39ce27a1da291fbfa6be013
김소운 <sozzykim@naver.com>
2025-05-12 15:27:28 +0900

    Remove: 아직 안 쓰는 메서드 삭제

--- backend/src/main/java/com/conkiri/domain/chatbot/repository/ConcertNoticeRepository.java
+++ backend/src/main/java/com/conkiri/domain/chatbot/repository/ConcertNoticeRepository.java
@@ -1,12 +1,15 @@
 package com.conkiri.domain.chatbot.repository;
 
+import com.conkiri.domain.base.entity.Concert;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.conkiri.domain.chatbot.entity.ConcertNotice;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ConcertNoticeRepository extends JpaRepository<ConcertNotice, Long> {
-	// List<ConcertNotice> findByConcert(Concert concert);
 
-	// 추후 쓸 예정
-	// List<ConcertNotice> findConcertByConcertId(Long concertId);
 }

a162ac1d5d0a5da10e53bae7f2114cea27b74c30
김소운 <sozzykim@naver.com>
2025-05-12 10:39:31 +0900

    Style: 아직 필요없는 코드 주석 삭제

--- backend/src/main/java/com/conkiri/domain/base/repository/ConcertDetailRepository.java
+++ backend/src/main/java/com/conkiri/domain/base/repository/ConcertDetailRepository.java
@@ -5,9 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.conkiri.domain.base.entity.ConcertDetail;
 
 public interface ConcertDetailRepository extends JpaRepository<ConcertDetail, Long> {
-	// // 특정 콘서트에 속한 모든 상세 정보 조회
-	// List<ConcertDetail> findByConcert(Concert concert);
-	//
-	// // 콘서트 ID로 모든 상세 정보 조회
-	// List<ConcertDetail> findConcertByConcertId(Long concertId);
+
 }

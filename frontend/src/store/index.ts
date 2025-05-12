699db0b1398f02e1d3f0d87027f58bfd7b884fbb
최윤지 <ynco32@gmail.com>
2025-05-12 16:22:40 +0900

    Feat: 티켓팅 실전 모드 redux로 상태 관리 구현

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import queueReducer from './slices/queueSlice';
 import errorReducer from './slices/errorSlice';
+import ticketingSeatSlice from './slices/ticketingSeatSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     queue: queueReducer,
     error: errorReducer,
+
     // 다른 리듀서들 추가
   },
   // RTK 2.x에서는 미들웨어 설정이 약간 변경됨

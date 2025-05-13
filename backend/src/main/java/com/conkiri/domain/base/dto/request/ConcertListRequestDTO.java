ebda019b2c99001f4ad1c77539347f42a879e990
임강호 <gksmfthsus35@naver.com>
2025-05-13 11:01:20 +0900

    fix: dto 수정

--- backend/src/main/java/com/conkiri/domain/base/dto/request/ConcertListRequestDTO.java
+++ backend/src/main/java/com/conkiri/domain/base/dto/request/ConcertListRequestDTO.java
@@ -1,7 +1,6 @@
 package com.conkiri.domain.base.dto.request;
 
 import java.util.List;
-import java.util.Map;
 
 import com.conkiri.global.exception.ValidationMessage;
 
@@ -11,12 +10,6 @@ public record ConcertListRequestDTO(
 
 	@NotNull(message = ValidationMessage.NULL_IS_NOT_ALLOWED)
 	List<Long> concertIds,
-	List<Long> concertDetailIds,
-	Map<Long, Boolean> entranceNotifications
+	List<Long> concertDetailIds
 ) {
-
-	public boolean isEntranceNotificationEnabled(Long concertDetailId) {
-		if (entranceNotifications == null) return false;
-		return entranceNotifications.getOrDefault(concertDetailId, false);
-	}
 }

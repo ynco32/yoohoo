f47e61b772b89afe6933ceb5e2d07ea30212a626
ynco32 <ynco32@sogang.ac.kr>
2025-05-17 00:53:06 +0900

    Fix: api 코드 형식 변경

--- frontend/src/api/notification/notification.ts
+++ frontend/src/api/notification/notification.ts
@@ -1,48 +1,36 @@
-import { apiRequest } from '../api';
+// 전체 코드
+import { apiClient } from '../api';
 import { Notification } from '@/types/notification';
 
+export const notificationApi = {
+  /**
+   * 전체 알림 조회
+   */
+  getAllNotification: () =>
+    apiClient.get<Notification[]>('/api/v1/notifications'),
 
-/**
- * 
- * @returns 전체 알림 조회
- */
-export const getAllNotification = async () => {
-  return apiRequest<Notification[]>('GET', '/api/v1/notifications');
-};
+  /**
+   * 안 읽은 알림이 있는지 조회
+   */
+  hasUnreadNotification: () =>
+    apiClient.get<boolean>('/api/v1/notifications/unread'),
 
+  /**
+   * 알림 권한 확인
+   */
+  checkNotificationAccess: () =>
+    apiClient.get<boolean>('/api/v1/notifications/settings'),
 
-/**
- * 
- * @returns 안 읽은 알림이 있는지 조회 
- */
-export const hasUnreadNotification = async () => {
-  return apiRequest<boolean>('GET', 'api/v1/notifications/unread');
-};
+  /**
+   * 특정 알림 읽음 처리
+   * @param notificationId 알림 아이디
+   */
+  readNotification: (notificationId: string) =>
+    apiClient.patch<null>(`/api/v1/notifications/${notificationId}/read`),
 
-/**
- * 
- * @returns 알림 권한 확인
- */
-export const checkNotificationAccess = async () => {
-  return apiRequest<boolean>('GET', '/api/v1/notifications/settings');
-};
-
-/**
- * 
- * @param notificationId 알림 아이디
- * @returns 읽음 처리
- */
-export const readNotification = async (notificationId: string) => {
-  return apiRequest<null>(
-    'PATCH',
-    `/api/v1/notifications/${notificationId}/read`
-  );
-};
-
-/**
- * 
- * @returns 전체 알림 읽음 처리
- */
-export const readAllNotificatios = async () => {
-  return apiRequest<null>('PATCH', '/api/v1/notifications/read-all');
+  /**
+   * 전체 알림 읽음 처리
+   */
+  readAllNotifications: () =>
+    apiClient.patch<null>('/api/v1/notifications/read-all'),
 };

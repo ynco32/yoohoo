82385ecef4651af34e67d20330d5bc1d669cffac
ynco32 <ynco32@sogang.ac.kr>
2025-05-17 00:47:39 +0900

    Chore: 주석 추가

--- frontend/src/api/notification/notification.ts
+++ frontend/src/api/notification/notification.ts
@@ -1,19 +1,37 @@
-import { apiClient, apiRequest } from '../api';
+import { apiRequest } from '../api';
 import { Notification } from '@/types/notification';
-import { ApiResponse } from '@/types/api';
 
+
+/**
+ * 
+ * @returns 전체 알림 조회
+ */
 export const getAllNotification = async () => {
   return apiRequest<Notification[]>('GET', '/api/v1/notifications');
 };
 
+
+/**
+ * 
+ * @returns 안 읽은 알림이 있는지 조회 
+ */
 export const hasUnreadNotification = async () => {
   return apiRequest<boolean>('GET', 'api/v1/notifications/unread');
 };
 
+/**
+ * 
+ * @returns 알림 권한 확인
+ */
 export const checkNotificationAccess = async () => {
   return apiRequest<boolean>('GET', '/api/v1/notifications/settings');
 };
 
+/**
+ * 
+ * @param notificationId 알림 아이디
+ * @returns 읽음 처리
+ */
 export const readNotification = async (notificationId: string) => {
   return apiRequest<null>(
     'PATCH',
@@ -21,6 +39,10 @@ export const readNotification = async (notificationId: string) => {
   );
 };
 
+/**
+ * 
+ * @returns 전체 알림 읽음 처리
+ */
 export const readAllNotificatios = async () => {
   return apiRequest<null>('PATCH', '/api/v1/notifications/read-all');
 };
